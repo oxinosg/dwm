5dc2cf5d5ae4141b87f1d52e2118d4318fcb3cb8
oxinosg <oxinosg@gmail.com>
2020-11-01 01:03:06 +0100

    col mode gappx

--- dwm.c
+++ dwm.c
@@ -2278,28 +2278,31 @@ tagmon(const Arg *arg)
 
 void
 col(Monitor *m) {
-	unsigned int i, n, h, w, x, y,mw;
-	Client *c;
+  unsigned int i, n, h, w, x, y, mw;
+  Client *c;
 
-	for(n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++);
-	if(n == 0)
-		return;
-        if(n > m->nmaster)
-                mw = m->nmaster ? m->ww * m->mfact : 0;
-        else
-                mw = m->ww;
-	for(i = x = y = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++) {
-		if(i < m->nmaster) {
-			 w = (mw - x) / (MIN(n, m->nmaster)-i);
-                         resize(c, x + m->wx, m->wy, w - (2*c->bw), m->wh - (2*c->bw), False);
-			x += WIDTH(c);
-		}
-		else {
-			h = (m->wh - y) / (n - i);
-			resize(c, x + m->wx, m->wy + y, m->ww - x  - (2*c->bw), h - (2*c->bw), False);
-			y += HEIGHT(c);
-		}
-	}
+  for(n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++);
+  if(n == 0)
+    return;
+
+  if(n > m->nmaster)
+    mw = m->nmaster ? m->ww * m->mfact : 0;
+  else
+    mw = m->ww - m->gappx;
+
+  for(i = 0, x = y = m->gappx, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++) {
+    if(i < m->nmaster) {
+      w = (mw - x) / (MIN(n, m->nmaster) - i);
+      resize(c, x + m->wx, m->wy + m->gappx, w - (2*c->bw), m->wh - (2*c->bw) - 2*m->gappx, False);
+      if (x + WIDTH(c) + m->gappx < m->ww)
+        x += WIDTH(c) + m->gappx;
+    } else {
+      h = (m->wh - y) / (n - i) - m->gappx;
+      resize(c, x + m->wx, m->wy + y, m->ww - x - (2*c->bw) - m->gappx, h - (2*c->bw), False);
+      if (y + HEIGHT(c) + m->gappx < m->wh)
+        y += HEIGHT(c) + m->gappx;
+    }
+  }
 }
 
 void
